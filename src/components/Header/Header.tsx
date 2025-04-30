7750c162d08649aff6410330331ff00c740d5f06
giovanimuller <mullersilva1515@gmail.com>
2025-04-30 12:50:26 -0300

    fix header bag icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,16 +10,16 @@ const Header: React.FC = () => {
       <div className="container">
         <div className="header-content">
           <div className="logo">
-            <img src="/img/logo-header.svg" alt="Logo Cadastra" />
+            <img src="/src/img/logo-header.svg" alt="Logo Cadastra" />
           </div>
           <div className="shopping-bag">
             {itemCount > 0 ? (
               <div className="cart-with-items">
-                <img src="img/ico-bag.svg" alt="Carrinho de compras com itens" />
+                <img src="/src/img/ico-bag.svg" alt="Carrinho de compras com itens" />
                 <span className="cart-count">{itemCount}</span>
               </div>
             ) : (
-              <img src="img/ico-bag.svg" alt="Carrinho de compras vazio" />
+              <img src="/src/img/ico-bag.svg" alt="Carrinho de compras vazio" />
             )}
           </div>
         </div>
